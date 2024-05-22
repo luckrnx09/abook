c2521669cb58b1e7d4c51caa77a96b5c4933513a
luckrnx09 <luckrnx09@gmail.com>
2024-05-22 23:54:12 +0800

    compute the task

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { Composer } from './composer';
 import {prepareBook} from './meta';
 
 const metaFile = process.cwd() + '/src/example.json';
 (async () => {
   const book = await prepareBook(metaFile);
-  console.log(JSON.stringify(book, null, 2));
+    //   console.log(JSON.stringify(book, null, 2));
+    const composer = new Composer(book);
+    composer.continueToWrite();
 })();
