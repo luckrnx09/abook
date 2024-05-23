26705b56566e09addf23fa9a5f5dc6e9e9b65f01
luckrnx09 <luckrnx09@gmail.com>
2024-05-23 17:19:08 +0800

    update

--- src/llm/openai.ts
+++ src/llm/openai.ts
@@ -1,12 +1,13 @@
 import {OpenAI} from '@langchain/openai';
 
 const openAIModel = new OpenAI({
-  temperature: 0.7,
-  model: process.env.OPENAI_MODEL,
-  configuration: {
-    baseURL: process.env.OPENAI_BASE_URL,
-  },
-  apiKey: process.env.OPENAI_API_KEY,
+    temperature: 0.7,
+    model: process.env.OPENAI_MODEL,
+    configuration: {
+        baseURL: process.env.OPENAI_BASE_URL,
+    },
+    verbose: process.env.VERBOSE === 'true',
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 export {openAIModel};
