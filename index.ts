326ba21d89b243532079df2a707bc75f649f2f39
luckrnx09 <luckrnx09@gmail.com>
2024-05-23 17:23:15 +0800

    remove python files

--- index.ts
+++
@@ -1,6 +0,0 @@
-const world = 'world';
-import fs from 'fs'
-export function hello(who: string = world): string {
-    console.log(fs)
-    return `Hello ${who}! `;
-}
--- llm/openai.py
+++
@@ -1,5 +0,0 @@
-from langchain.chat_models.openai import ChatOpenAI
-import os
-
-llm = ChatOpenAI(model_name=os.getenv('OPENAI_MODEL', 'gpt-3.5-turbo-16k'),
-                 streaming=False, verbose=True)
