1c7edcf5101a20a7d298f7ab342bae8f45eb35a4
luckrnx09 <luckrnx09@gmail.com>
2024-06-23 13:37:49 +0800

    update

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 import * as path from 'path';
 import {pkg} from './pkg';
+import * as os from 'os';
 
-const BOOK_OUTPUT_DIR = path.resolve(process.cwd(), `.${pkg.name}`, 'books');
-const IDEA_OUTPUT_DIR = path.resolve(process.cwd(), `.${pkg.name}`, 'ideas');
+const BOOK_OUTPUT_DIR = path.resolve(os.homedir(), `.${pkg.name}`, 'books');
+const IDEA_OUTPUT_DIR = path.resolve(os.homedir(), `.${pkg.name}`, 'ideas');
 
 export {BOOK_OUTPUT_DIR, IDEA_OUTPUT_DIR};
